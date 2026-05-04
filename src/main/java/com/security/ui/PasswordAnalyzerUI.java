49e55896971120704cd634b7ad45123402d20bce
RealGevorgian <gevorgvgevorgyan@gmail.com>
2026-05-04 19:36:45 +0400

    Deleting redundant commented testing code

--- src/main/java/com/security/ui/PasswordAnalyzerUI.java
+++ src/main/java/com/security/ui/PasswordAnalyzerUI.java
@@ -1,212 +1,3 @@
-//package com.security.ui;
-//
-//import com.security.analyzer.AnalysisResult;
-//import com.security.analyzer.PasswordAnalyzer;
-//import com.security.features.CrackTimeEstimator;
-//import com.security.features.PassphraseGenerator;
-//import com.security.features.PasswordReuseChecker;
-//
-//import javax.swing.*;
-//import javax.swing.event.DocumentEvent;
-//import javax.swing.event.DocumentListener;
-//import java.awt.*;
-//
-//public class PasswordAnalyzerUI extends JFrame{
-//
-//    private JTextField passwordField;
-//    private JTextArea resultArea;
-//    private JCheckBox reuseCheckBox;
-//    private JComboBox<String> attackModeBox;
-//    private JProgressBar strengthBar;
-//    private JLabel statusLabel;
-//
-//    private PasswordAnalyzer analyzer;
-//
-//    private final Color BG_COLOR = new Color(30, 30, 30);
-//    private final Color PANEL_COLOR = new Color(45, 45, 45);
-//    private final Color TEXT_COLOR = new Color(220, 220, 220);
-//
-//    public PasswordAnalyzerUI(){
-//
-//        analyzer = new PasswordAnalyzer();
-//
-//        setTitle("Password Strength Analyzer");
-//        setSize(560, 560);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setLocationRelativeTo(null);
-//
-//        setLayout(new BorderLayout());
-//        getContentPane().setBackground(BG_COLOR);
-//
-//        JPanel topPanel = new JPanel(new GridLayout(6, 1, 5, 5));
-//        topPanel.setBackground(PANEL_COLOR);
-//
-//        JLabel label = new JLabel("Please Enter Password:");
-//        label.setFont(new Font("Arial", Font.BOLD, 14));
-//        label.setForeground(TEXT_COLOR);
-//
-//        passwordField = new JTextField();
-//        passwordField.setBackground(new Color(60, 60, 60));
-//        passwordField.setForeground(TEXT_COLOR);
-//        passwordField.setCaretColor(TEXT_COLOR);
-//        passwordField.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
-//
-//        reuseCheckBox = new JCheckBox("I reuse this password on multiple accounts");
-//        reuseCheckBox.setBackground(PANEL_COLOR);
-//        reuseCheckBox.setForeground(TEXT_COLOR);
-//
-//        attackModeBox = new JComboBox<>(new String[]{
-//                "Normal Attacker",
-//                "GPU Attacker"
-//        });
-//        attackModeBox.setBackground(PANEL_COLOR);
-//        attackModeBox.setForeground(TEXT_COLOR);
-//
-//        strengthBar = new JProgressBar(0, 100);
-//        strengthBar.setStringPainted(true);
-//        strengthBar.setBackground(new Color(60, 60, 60));
-//
-//        statusLabel = new JLabel("SECURITY STATUS: -");
-//        statusLabel.setFont(new Font("Arial", Font.BOLD, 18));
-//        statusLabel.setHorizontalAlignment(SwingConstants.CENTER);
-//        statusLabel.setForeground(TEXT_COLOR);
-//
-//        topPanel.add(label);
-//        topPanel.add(passwordField);
-//        topPanel.add(reuseCheckBox);
-//        topPanel.add(attackModeBox);
-//        topPanel.add(strengthBar);
-//        topPanel.add(statusLabel);
-//
-//        resultArea = new JTextArea();
-//        resultArea.setEditable(false);
-//        resultArea.setFont(new Font("Monospaced", Font.PLAIN, 16));
-//        resultArea.setBackground(new Color(20, 20, 20));
-//        resultArea.setForeground(TEXT_COLOR);
-//
-//        JScrollPane scrollPane = new JScrollPane(resultArea);
-//        scrollPane.getViewport().setBackground(new Color(20, 20, 20));
-//
-//        JPanel bottomPanel = new JPanel();
-//        bottomPanel.setBackground(PANEL_COLOR);
-//
-//        JButton analyzeButton = new JButton("Analyze");
-//        JButton generateButton = new JButton("Generate Passphrase");
-//
-//        styleButton(analyzeButton);
-//        styleButton(generateButton);
-//
-//        bottomPanel.add(analyzeButton);
-//        bottomPanel.add(generateButton);
-//
-//        add(topPanel, BorderLayout.NORTH);
-//        add(scrollPane, BorderLayout.CENTER);
-//        add(bottomPanel, BorderLayout.SOUTH);
-//
-//        analyzeButton.addActionListener(e -> analyzePassword());
-//        generateButton.addActionListener(e -> generatePassphrase());
-//
-//        passwordField.getDocument().addDocumentListener(new DocumentListener() {
-//            public void insertUpdate(DocumentEvent e) { analyzePassword(); }
-//            public void removeUpdate(DocumentEvent e) { analyzePassword(); }
-//            public void changedUpdate(DocumentEvent e) { analyzePassword(); }
-//        });
-//    }
-//
-//    private void styleButton(JButton button) {
-//        button.setBackground(new Color(70, 70, 70));
-//        button.setForeground(TEXT_COLOR);
-//        button.setFocusPainted(false);
-//    }
-//
-//    private void animateProgressBar(int targetValue) {
-//        Timer timer = new Timer(10, null);
-//
-//        timer.addActionListener(e ->{
-//            int current = strengthBar.getValue();
-//
-//            if(current < targetValue){
-//                strengthBar.setValue(current + 1);
-//            }else if (current > targetValue){
-//                strengthBar.setValue(current - 1);
-//            } else{
-//                timer.stop();
-//            }
-//        });
-//
-//        timer.start();
-//    }
-//
-//    private void analyzePassword(){
-//
-//        String password = passwordField.getText();
-//
-//        if(password.isEmpty()){
-//            resultArea.setText("Please enter a password.");
-//            strengthBar.setValue(0);
-//            statusLabel.setText("SECURITY STATUS: -");
-//            passwordField.setBorder(BorderFactory.createLineBorder(Color.GRAY, 2));
-//            return;
-//        }
-//
-//        AnalysisResult result = analyzer.analyze(password);
-//
-//        animateProgressBar(result.getScore());
-//        strengthBar.setString(result.getScore() + "/100");
-//
-//        if(result.getStrength().toString().equals("WEAK")){
-//            resultArea.setForeground(Color.RED);
-//            strengthBar.setForeground(Color.RED);
-//            statusLabel.setForeground(Color.RED);
-//            passwordField.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
-//
-//        } else if(result.getStrength().toString().equals("MEDIUM")){
-//            resultArea.setForeground(Color.ORANGE);
-//            strengthBar.setForeground(Color.ORANGE);
-//            statusLabel.setForeground(Color.ORANGE);
-//            passwordField.setBorder(BorderFactory.createLineBorder(Color.ORANGE, 2));
-//
-//        } else{
-//            Color strongGreen = new Color(0, 200, 0);
-//
-//            resultArea.setForeground(strongGreen);
-//            strengthBar.setForeground(strongGreen);
-//            statusLabel.setForeground(strongGreen);
-//            passwordField.setBorder(BorderFactory.createLineBorder(strongGreen, 2));
-//        }
-//
-//        statusLabel.setText("SECURITY STATUS: " + result.getStrength());
-//
-//        boolean isReused = reuseCheckBox.isSelected();
-//        String reuseMessage = PasswordReuseChecker.check(isReused);
-//
-//        String selected = (String) attackModeBox.getSelectedItem();
-//
-//        CrackTimeEstimator.AttackMode mode =
-//                selected.equals("GPU Attacker")
-//                        ? CrackTimeEstimator.AttackMode.GPU
-//                        : CrackTimeEstimator.AttackMode.NORMAL;
-//
-//        String crackTime = CrackTimeEstimator.estimate(password, mode);
-//
-//        resultArea.setText(
-//                "Strength: " + result.getStrength() + "\n" +
-//                        "Score: " + result.getScore() + "/100\n" +
-//                        "Entropy: " + String.format("%.2f", result.getEntropy()) + " bits\n" +
-//                        "Approximate Crack Time: " + crackTime + "\n" +
-//                        "Attack Type: " + result.getAttackType() + "\n" +
-//                        "Feedback: " + result.getFeedback() + "\n\n" +
-//                        "Reuse Risk: " + reuseMessage
-//        );
-//    }
-//
-//    private void generatePassphrase(){
-//        String passphrase = PassphraseGenerator.generate();
-//        passwordField.setText(passphrase);
-//    }
-//}
-
-
 package com.security.ui;
 
 import com.security.analyzer.AnalysisResult;
@@ -220,7 +11,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.*;
 
-public class PasswordAnalyzerUI extends JFrame {
+public class PasswordAnalyzerUI extends JFrame{
 
     private JTextField passwordField;
     private JTextArea resultArea;
@@ -235,7 +26,7 @@ public class PasswordAnalyzerUI extends JFrame {
     private final Color PANEL_COLOR = new Color(45, 45, 45);
     private final Color TEXT_COLOR = new Color(220, 220, 220);
 
-    public PasswordAnalyzerUI() {
+    public PasswordAnalyzerUI(){
 
         analyzer = new PasswordAnalyzer();
 
@@ -334,14 +125,14 @@ public class PasswordAnalyzerUI extends JFrame {
         analyzeButton.addActionListener(e -> analyzePassword());
         generateButton.addActionListener(e -> generatePassphrase());
 
-        passwordField.getDocument().addDocumentListener(new DocumentListener() {
-            public void insertUpdate(DocumentEvent e) { analyzePassword(); }
-            public void removeUpdate(DocumentEvent e) { analyzePassword(); }
-            public void changedUpdate(DocumentEvent e) { analyzePassword(); }
+        passwordField.getDocument().addDocumentListener(new DocumentListener(){
+            public void insertUpdate(DocumentEvent e){ analyzePassword();}
+            public void removeUpdate(DocumentEvent e){ analyzePassword();}
+            public void changedUpdate(DocumentEvent e){ analyzePassword();}
         });
     }
 
-    private void styleButton(JButton button) {
+    private void styleButton(JButton button){
         button.setBackground(new Color(70, 70, 70));
         button.setForeground(TEXT_COLOR);
         button.setFocusPainted(false);
@@ -349,10 +140,10 @@ public class PasswordAnalyzerUI extends JFrame {
         button.setFont(new Font("Arial", Font.BOLD, 13));
     }
 
-    private void animateProgressBar(int targetValue) {
+    private void animateProgressBar(int targetValue){
         Timer timer = new Timer(10, null);
 
-        timer.addActionListener(e -> {
+        timer.addActionListener(e ->{
             int current = strengthBar.getValue();
 
             if (current < targetValue) {
@@ -367,11 +158,11 @@ public class PasswordAnalyzerUI extends JFrame {
         timer.start();
     }
 
-    private void analyzePassword() {
+    private void analyzePassword(){
 
         String password = passwordField.getText();
 
-        if (password.isEmpty()) {
+        if(password.isEmpty()){
             resultArea.setText("Please enter a password.");
             strengthBar.setValue(0);
             statusLabel.setText("SECURITY STATUS: -");
@@ -384,11 +175,11 @@ public class PasswordAnalyzerUI extends JFrame {
         animateProgressBar(result.getScore());
         strengthBar.setString(result.getScore() + "/100");
 
-        if (result.getStrength().toString().equals("WEAK")) {
+        if(result.getStrength().toString().equals("WEAK")){
             applyThemeColor(Color.RED);
-        } else if (result.getStrength().toString().equals("MEDIUM")) {
+        } else if(result.getStrength().toString().equals("MEDIUM")){
             applyThemeColor(Color.ORANGE);
-        } else {
+        } else{
             applyThemeColor(new Color(0, 200, 0));
         }
 
@@ -417,14 +208,14 @@ public class PasswordAnalyzerUI extends JFrame {
         );
     }
 
-    private void applyThemeColor(Color color) {
+    private void applyThemeColor(Color color){
         resultArea.setForeground(color);
         strengthBar.setForeground(color);
         statusLabel.setForeground(color);
         passwordField.setBorder(BorderFactory.createLineBorder(color, 2));
     }
 
-    private void generatePassphrase() {
+    private void generatePassphrase(){
         String passphrase = PassphraseGenerator.generate();
         passwordField.setText(passphrase);
     }
